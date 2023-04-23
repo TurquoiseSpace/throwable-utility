0c0e4f8aee7496f75c90e056c808476a40abc2a1
Prakhar Makhija <matcdac@gmail.com>
2023-04-24 02:02:25 +0530

    adding constants

---
+++ src/main/java/com/TurquoiseSpace/constant/ThrowableConstants.java
@@ -0,0 +1,9 @@
+package com.TurquoiseSpace.constant;
+
+public interface ThrowableConstants {
+
+	public static final String THROWABLE_ENCOUNTERED = "Encountered Throwable -> (class) {} (message) {}";
+
+	public static final String THROWABLE_PARSED = "Parsed Throwable -> (json) {}";
+
+}
